38ac775891fed29d60967f593929ada4c8eb4564
wxliubb <bei.liu@thingsx.dev>
2024-01-05 09:59:35 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/kotlinlearn/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlearn/MainActivity.kt
@@ -9,6 +9,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("kotlin","Hello World")
+        Log.d("kotlin", "Hello World")
+        // 1.val var
+        // 2.
+
+        // 2.4 程序的控制语句
+        fun largeNumber(num1: Int, num2: Int): Int {
+//            var value = 0;
+//            if (num1 > num2) {
+//                value = num1
+//            } else {
+//                value = num2
+//            }
+
+            // 简化 1
+            var value = if (num1 > num2) {
+                num1
+            } else {
+                num2
+            }
+            return value;
+        }
+
     }
 }
